43f84759eb6ac0ca8c16f7a8b5dbc219a2c94721
Anil Patil <34886133+patil8686212067@users.noreply.github.com>
2021-02-08 01:36:34 +0530

    Added some  sop in git
    Added some sop in git

--- src/main/java/com/ashokit/test/JenkinDemo.java
+++ src/main/java/com/ashokit/test/JenkinDemo.java
@@ -4,6 +4,9 @@ public class JenkinDemo {
 
 	public static void main(String[] args) {
 		System.out.println("--This class is for Jenkin Demo--");
+		
+		System.out.println("-- I am doing chnages in github ,and i need to verify this chnages related notification in Jenkins--");
+
 
 	}
 
